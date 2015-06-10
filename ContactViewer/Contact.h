e3149d10c01f52fd24d4f319bffffc5c39da9529
Elliot <elliot.schrock@gmail.com>
2015-06-10 18:46:43 -0400

    restore Contact model

---
+++ ContactViewer/Contact.h
@@ -0,0 +1,14 @@
+//
+//  Contact.h
+//  ContactViewer
+//
+//  Created by Elliot Schrock on 6/10/15.
+//  Copyright (c) 2015 Elliot Schrock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contact : NSObject
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *phoneNumber;
+@end
